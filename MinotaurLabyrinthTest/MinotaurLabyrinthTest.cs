29e8ed8ff78930bf30134b7708165ffa6156111f
Kripesh Thapaliya <Kripeshthapaliya@gmail.com>
2023-06-18 13:52:51 -0400

    Add batman

--- MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
+++ MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
@@ -6,9 +6,72 @@ namespace MinotaurLabyrinthTest
     public class Tests
     {
         [TestMethod]
-        public void DummyTest()
+        public void PitRoomTest()
         {
-            Assert.AreNotSame(1, 2);
+            Pit pitRoom = new Pit();
+            Hero hero = new Hero();
+            Map map = new Map(1, 1);
+
+            pitRoom.Activate(hero, map);
+            Assert.AreEqual(pitRoom.IsActive, false);
+            Assert.AreEqual(hero.IsAlive, true);
+
+            hero.HasSword = true;
+            pitRoom.Activate(hero, map);
+            Assert.AreEqual(hero.IsAlive, true);
+
+            Pit newPitRoom = new Pit();
+            newPitRoom.Activate(hero, map);
+            Assert.AreEqual(hero.IsAlive, false);
+        }
+
+        [TestMethod]
+        public void Batmantest()
+        {
+            BatmanRoom batmanGathering = new BatmanGathering();
+            Hero hero = new Hero();
+            hero.SetStealth(100);
+            Map map = new Map(9, 9);
+
+            batmanGathering.Activate(hero, map);
+            Assert.IsFalse(batmanGathering.IsActive);
+            Assert.IsFalse(hero.IsAlive);
+
+            bool result = batmanGathering.DisplaySense(hero, 9);
+            Assert.IsTrue(result);
+            result = batmanGathering.DisplaySense(hero, 9);
+            Assert.IsFalse(result);
+        }
+
+        private class BatmanGathering : BatmanRoom
+        {
+        }
+    }
+
+    [TestClass]
+    public class MonsterTests
+    {
+        [TestMethod]
+        public void MinotaurTest()
+        {
+            Hero hero = new Hero();
+            Minotaur minotaur = new Minotaur();
+            Map map = new Map(4, 4);
+            hero.HasSword = true;
+            Assert.AreEqual(hero.HasSword, true);
+
+            minotaur.Activate(hero, map);
+            // Charge moves the hero 1 room east and 2 rooms north
+            // -1 is off the map so hero position should be (0, 2)
+            Assert.AreEqual(hero.Location, new Location(0, 2));
+            Assert.AreEqual(hero.HasSword, false);
+
+            minotaur.Activate(hero, map);
+            Assert.AreEqual(hero.Location, new Location(0, 3));
+
+            hero.Location = new Location(3, 1);
+            minotaur.Activate(hero, map);
+            Assert.AreEqual(hero.Location, new Location(2, 3));
         }
     }
 }
